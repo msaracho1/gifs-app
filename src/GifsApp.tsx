8bf012e8b7b743f0b44c0438c88a736a96194c00
Necroo1 <michaeldavidsg@gmail.com>
2026-01-29 20:40:30 -0300

    feat: add gif mocks and split into components

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,6 +1,8 @@
 import { SearchBar } from "./shared/components/SearchBar";
 import { mockGifs } from "./mock-data/gifs.mock";
 import { CustomHeader } from "./shared/components/CustomHeader";
+import { PreviousSearches } from "./gifs/components/PreviousSearches";
+import { GifsList } from "./gifs/components/GifsList";
 
 export const GifsApp = () => {
   return (
@@ -8,31 +10,11 @@ export const GifsApp = () => {
     {/* Header */}
     <CustomHeader title="Buscador de Gifs" description="Descubre y comparte los mejores gifs" />
     {/* search */}
-       <SearchBar placeholder="Busca tus gifs favoritos" buttonText="Buscar Gifs" />
+    <SearchBar placeholder="Busca tus gifs favoritos" buttonText="Buscar Gifs" />
     {/* Búsquedas previsas */}
-    <div className="previous-searches">
-        <h2>Búsquedas Previas</h2>
-        <ul className="previous-searches-list">
-            <li>Gatos</li>
-            <li>Perros</li>
-            <li>Memes</li>
-            <li>Reacciones</li>
-        </ul>
-    </div>
+    <PreviousSearches searches={['Gatos', 'Perros', 'Memes', 'Reacciones']} />
     {/* Show gifs */}
-    <div className="gifs-container">
-        {
-           mockGifs.map((gif)=>(
-            <div key={gif.id} className="gif-card">
-                <img src={gif.url} alt={gif.title} />
-                <h3>{gif.title}</h3>
-                <p>
-                    {gif.width} x {gif.height} (1.5mb)
-                </p>
-            </div>
-           ))
-        }
-    </div>
+    <GifsList gifs={mockGifs} />
     </>
   );
 }
